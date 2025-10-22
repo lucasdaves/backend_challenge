57724fb53aa653a6f2e93bfe4d1c89ae8112dfc1
Lucas Guli <lucas.guli@hotmail.com>
2025-10-21 21:01:19 -0300

    removed express middleware

--- src/services/express/app.ts
+++ src/services/express/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "./routes/routes.js";
-import expressMiddleware from "../middlewares/express.middleware.js";
+import cors from "cors";
 import { loggerMiddleware } from "../middlewares/logger.middleware.js";
 import { errorMiddleware } from "../middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(expressMiddleware);
+app.use(cors());
+app.use(express.json());
 app.use(loggerMiddleware);
 app.use(routes);
 app.use(errorMiddleware);
